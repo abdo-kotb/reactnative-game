a71f8690e0dce4636b57071bc5e1bd6659b60b85
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-01-10 17:10:29 +0400

    style: ImageBackground component

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native'
+import { ImageBackground, StyleSheet, View } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 
 import StartGameScreen from './screens/StartGame.screen'
@@ -6,7 +6,14 @@ import StartGameScreen from './screens/StartGame.screen'
 export default function App() {
   return (
     <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   )
 }
@@ -14,5 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 })

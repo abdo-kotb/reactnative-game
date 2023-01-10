21544139f5bf598fd50206ff18439283f248e177
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-01-10 16:24:19 +0400

    style: feedback to button press

--- components/Primary.button.tsx
+++ components/Primary.button.tsx
@@ -1,16 +1,48 @@
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 
 interface Props {
   children: string
 }
 
 const PrimaryButton: FC<Props> = ({ children }) => {
+  const pressHandler = () => {}
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.buttonInnerContainer,
+          pressed && styles.pressed,
+        ]}
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   )
 }
 
 export default PrimaryButton
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: '#ffffff',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+})
--- screens/StartGame.screen.tsx
+++ screens/StartGame.screen.tsx
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: '#4e0329',
     borderRadius: 8,
     // android-specific shadow
     elevation: 4,
